c526f9d7099cc2a05cf0a22c2e898da26409424c
gofastpanam <8972@holbertonstudents.com>
2024-06-11 14:34:50 +0200

    .

--- app/models/place.py
+++ app/models/place.py
@@ -9,6 +9,7 @@ class Place(BaseModel):
     def __init__(self, name, description, address, city_id,
                  latitude, longitude, host_id, num_rooms,
                  num_bathrooms, price_per_night, max_guest):
+        super().__init__()
         self.name = name
         self.description = description
         self.address = address
@@ -20,3 +21,19 @@ class Place(BaseModel):
         self.num_bathrooms = num_bathrooms
         self.price_per_night = price_per_night
         self.max_guests = max_guest
+
+    def to_dict(self):
+        return {
+            'id': str(self.id),
+            'name': self.name,
+            'description': self.description,
+            'adress': self.address,
+            'city_id': self.city_id,
+            'host_id': self.host_id,
+            'latitude': self.latitude,
+            'longitude': self.longitude,
+            'num_rooms': self.num_rooms,
+            'num_bathrooms': self.num_bathrooms,
+            'price_per_night': self.price_per_night,
+            
+        }
